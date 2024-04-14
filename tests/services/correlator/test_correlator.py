9943a6550af5bf3c9fe329594fcf98a004e35d55
HRAshton <12210721+hrashton@users.noreply.github.com>
2024-04-14 15:08:51 +0200

    Adjust correlator tests for vary precision

--- tests/services/correlator/test_correlator.py
+++ tests/services/correlator/test_correlator.py
@@ -3,7 +3,7 @@ import math
 import cupy as cp
 import numpy as np
 
-from series_opening_recognizer.configuration import RATE
+from series_opening_recognizer.configuration import RATE, PRECISION_SECS
 from series_opening_recognizer.services.correlator.correlator import calculate_correlation
 
 
@@ -16,7 +16,6 @@ def test_integration_returns_none_when_no_correlation():
     assert result is None
 
 
-# noinspection PyTypeChecker
 def test_integration_calculates_correctly():
     offset1 = int(4.2 * RATE)
     offset2 = int(7.3 * RATE)
@@ -30,21 +29,26 @@ def test_integration_calculates_correctly():
 
     result = calculate_correlation(audio1, audio2)
 
-    assert cp.isclose(result[0], cp.array(0))
-    assert cp.isclose(result[1], cp.array(3.1))
-    assert result[2].shape[0] == 26  # What is the size of the result[2]?
-    assert result[2].shape[1] == 2
+    precision_beats_multiplier = RATE * PRECISION_SECS
 
-    expected_beat_values = cp.arange(0, 26, 1) * RATE
-    observed_beat_values = result[2][:, 0].get()
-    assert np.allclose(observed_beat_values, expected_beat_values)
+    assert cp.isclose(result[0], cp.array(0)), "Audio 1 should have a correct offset"
+    assert cp.isclose(result[1], cp.array(3.1)), "Audio 2 should have a correct offset"
+    assert result[2].shape[0] == int((30 - (7.3 - 4.2)) / PRECISION_SECS), "Correlation should have correct size"
+    assert result[2].shape[1] == 2, "Correlation should have 2 columns"
 
-    values = result[2][:, 1].get()
+    corr = result[2].get()
+    observed_beat_values = corr[:, 0]
+    expected_beat_values = cp.arange(0, observed_beat_values.size, 1) * precision_beats_multiplier
+    assert np.allclose(observed_beat_values, expected_beat_values), "Correlation should have correct indices"
+
+    values = corr[:, 1]
     mean = np.mean(values)
 
+    idx_start = math.floor(offset1 / precision_beats_multiplier)
+    idx_end = math.ceil((offset1 + common_part_size) / precision_beats_multiplier)
     peak_mask = np.zeros(values.shape[0], dtype=bool)
-    peak_mask[math.floor(offset1 / RATE):math.ceil((offset1 + common_part_size) / RATE)] = True
-    assert np.all(values[peak_mask] > mean)
+    peak_mask[idx_start:idx_end] = True
+    assert np.all(values[peak_mask] > mean), "Peak values should be higher than mean"
 
     non_peak_mask = ~peak_mask
-    assert np.all(values[non_peak_mask] < mean)
+    assert np.all(values[non_peak_mask] < mean), "Non-peak values should be lower than mean"
